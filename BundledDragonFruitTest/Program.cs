224121d1fb808c6b5cfded31aaec6d9df641f53c
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2021-01-07 23:47:35 -0800

    Add project

---
+++ BundledDragonFruitTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BundledDragonFruitTest
+{
+    class Program
+    {
+        /// <summary>
+        /// Bundled app test
+        /// </summary>
+        /// <param name="someOption">Some option!</param>
+        /// <param name="anotherParameter">Just another parameter.</param>
+        /// <param name="tries">Number of retries (default = 2).</param>
+        static void Main(bool someOption, string anotherParameter, int tries = 2)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
